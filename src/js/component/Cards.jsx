006a95d6b035ab55a02a573ab5bc876906b2142b
Martín Pravia <marto.pravia@gmail.com>
2024-11-11 14:33:38 -0300

    actualizado Cards con toda la info y maquetado, faltan props

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -2,56 +2,99 @@ import React from "react";
 
 const Cards = () => {
   return (
-    <div className="row row-cols-1 row-cols-md-3 g-4">
-      <div className="col">
+    <div className="row row-cols-1  g-4 text-center">
+      <div className="col-3">
         <div className="card h-100">
-          <img src="..." className="card-img-top" alt="..." />
+          <img
+            src="https://picsum.photos/500/325"
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+              numquam aliquid sit quaerat enim totam architecto necessitatibus
+              iusto consequuntur voluptatem.
             </p>
           </div>
           <div className="card-footer">
             <small className="text-body-secondary">
-              Last updated 3 mins ago
+              <button type="button" class="btn btn-primary">
+                Find Out More!
+              </button>
             </small>
           </div>
         </div>
       </div>
-      <div className="col">
+      <div className="col-3">
         <div className="card h-100">
-          <img src="..." className="card-img-top" alt="..." />
+          <img
+            src="https://picsum.photos/500/325"
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
-              This card has supporting text below as a natural lead-in to
-              additional content.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+              iste dolorem quaerat sint unde similique, vitae delectus cumque
+              fugit debitis!
             </p>
           </div>
           <div className="card-footer">
             <small className="text-body-secondary">
-              Last updated 3 mins ago
+              <button type="button" class="btn btn-primary">
+                Find Out More!
+              </button>
             </small>
           </div>
         </div>
       </div>
-      <div className="col">
+      <div className="col-3">
         <div className="card h-100">
-          <img src="..." className="card-img-top" alt="..." />
+          <img
+            src="https://picsum.photos/500/325"
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptatum sed fugiat consectetur? Distinctio aliquid totam sequi
+              dignissimos vitae culpa ullam.
             </p>
           </div>
           <div className="card-footer">
             <small className="text-body-secondary">
-              Last updated 3 mins ago
+              <button type="button" class="btn btn-primary">
+                Find Out More!
+              </button>
+            </small>
+          </div>
+        </div>
+      </div>
+      <div className="col-3">
+        <div className="card h-100">
+          <img
+            src="https://picsum.photos/500/325"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+              voluptates aperiam nostrum laudantium tempore quaerat voluptate
+              ullam natus earum assumenda.
+            </p>
+          </div>
+          <div className="card-footer">
+            <small className="text-body-secondary">
+              <button type="button" class="btn btn-primary">
+                Find Out More!
+              </button>
             </small>
           </div>
         </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 import Navbar from "./NavBar";
 import JumbotronText from "./JumbotronText";
+import Cards from "./Cards";
 
 //create your first component
 const Home = () => {
   return (
     <>
       <Navbar />
+
       <div className="container">
         <JumbotronText />
+        <Cards />
         <div className="cards">Prueba</div>
         <div className="footer">Prueba</div>
       </div>
