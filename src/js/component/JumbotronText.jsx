14a89528969114b93975911b60644a9f7dbb30be
Martín Pravia <marto.pravia@gmail.com>
2024-11-11 15:32:12 -0300

    carga de props de Jumbotron, en Jumbotron y en home

--- src/js/component/JumbotronText.jsx
+++ src/js/component/JumbotronText.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 
-const JumbotronText = () => {
+const JumbotronText = ({ title, description, button }) => {
   return (
     <div class="col-md-12 card mb-3 bg-secondary">
       <div class=" card-body">
-        <h5 class="card-title display-2">Bienvenido al viaje de tu vida!</h5>
-        <p class="card-text">
-          En nuestra agencia de viajes, te ofrecemos los mejores paquetes para
-          que vivas experiencias únicas, con total respaldo y seguridad. Nos
-          encargamos de cada detalle, para que solo tengas que disfrutar. Confiá
-          en nuestro equipo y descubrí destinos increíbles sin contratiempos.
-          ¡Tu aventura perfecta empieza aquí!
-        </p>
+        <h5 class="card-title display-2">{title}</h5>
+        <p class="card-text">{description}</p>
         <a href="#" class="btn btn-primary">
-          Ver paquetes de viajes!
+          {button}
         </a>
       </div>
     </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,16 +7,27 @@ import Footer from "./Footer";
 
 //create your first component
 const Home = () => {
+  const jumbotronText = {
+    title: "Bienvenido al viaje de tu vida!",
+    description: `En nuestra agencia de viajes, te ofrecemos los mejores paquetes para
+          que vivas experiencias únicas, con total respaldo y seguridad. Nos
+          encargamos de cada detalle, para que solo tengas que disfrutar. Confiá
+          en nuestro equipo y descubrí destinos increíbles sin contratiempos.
+          ¡Tu aventura perfecta empieza aquí! `,
+    button: "Ver paquetes de viajes!",
+  };
+
   return (
     <>
       <Navbar />
 
       <div className="container">
-        <JumbotronText />
+        <JumbotronText {...jumbotronText} />
         <Cards />
       </div>
       <Footer />
     </>
   );
 };
+
 export default Home;
