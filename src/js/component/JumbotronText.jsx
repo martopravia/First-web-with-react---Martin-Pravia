747901bc84e4f5c8d452c8930e525c336982bb15
Martín Pravia <marto.pravia@gmail.com>
2024-11-11 14:22:15 -0300

    Creo Cards.jsx, comienzo a colocar contenido

--- src/js/component/JumbotronText.jsx
+++ src/js/component/JumbotronText.jsx
@@ -2,14 +2,17 @@ import React from "react";
 
 const JumbotronText = () => {
   return (
-    <div class="col-md-12 card mb-3">
+    <div class="col-md-12 card mb-3 bg-secondary">
       <div class=" card-body">
         <h5 class="card-title display-2">A Warm Welcome!</h5>
         <p class="card-text">
-          With supporting text below as a natural lead-in to additional content.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum alias
+          quidem quisquam, recusandae omnis repellendus odio aspernatur nihil
+          saepe optio fugiat officiis consequuntur architecto. Omnis quas totam
+          nostrum accusantium. Totam?
         </p>
         <a href="#" class="btn btn-primary">
-          Button
+          Call to action!
         </a>
       </div>
     </div>
